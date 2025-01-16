5f81fae4d7992dcfd9d85dc85ef8f5b324582340
mhanna <mandzyuk.annie@gmail.com>
2025-01-16 22:49:51 +0100

    Update dependencies and enhance UI components: Added 'clsx' for conditional class management, modified Tailwind configuration for improved styling, and refined button styles. Updated HomePage component to include a new navigation feature with a sliding panel.

--- src/componenets/button/Button.jsx
+++ src/componenets/button/Button.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types'
 
 const Button = ({ variant, children, onClick }) => {
    const variantClasses = {
-    primaryWhite: "bg-blue-lighter px-6 py-1 lg:px-9 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-white hover:text-blue-lighter text-white font-bold uppercase text-xl lg:text-2xl h",
-    primaryBlue: "bg-white text-blue-lighter px-6 py-1 lg:px-9 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-blue-lighter hover:text-white font-bold uppercase text-xl lg:text-2xl",
-    secondaryWhite: "bg-blue-lighter p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full text-white font-bold uppercase text-sm lg:text-base",
-    secondaryBlue: "bg-white text-blue-lighter font-bold uppercase p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-blue-lighter after:transition-all after:duration-300 hover:after:w-full text-sm lg:text-base",
+    primaryWhite: "bg-blue-lighter px-5 py-1 lg:px-7 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-white hover:text-blue-lighter text-white font-bold uppercase text-xl lg:text-2xl h",
+    primaryBlue: "bg-white text-blue-lighter px-5 py-1 lg:px-7 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-blue-lighter hover:text-white font-bold uppercase text-xl lg:text-2xl",
+    secondaryWhite: "p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full text-white font-bold uppercase text-sm lg:text-base",
+    secondaryBlue: "p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-blue-lighter after:transition-all after:duration-300 hover:after:w-full text-blue-lighter font-bold uppercase text-sm lg:text-base ",
    }
 
    return (
--- src/componenets/homePage/HomePage.jsx
+++ src/componenets/homePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import Button from '../button/Button'
 import Logo from '../../assets/logo.svg'
+import { useState } from 'react'
+import clsx from 'clsx'
 
 const HomePage = () => {
     const navigate = useNavigate();
@@ -9,34 +11,60 @@ const HomePage = () => {
         navigate('/game');
     };
 
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="h-screen w-screen">
-            <div className="mx-5 lg:mx-10 pt-5">
-                <Button 
-                    variant="secondaryWhite"
-                    onClick={handleNavigate}>
-                    How to play?
-                </Button>
-                <div className="flex flex-col items-center justify-center gap-8 lg:gap-4 mt-5">
-                    <h1>
-                        <img 
-                            src={Logo} 
-                            alt="logo" 
-                        />
-                    </h1>
-                    <span className="text-lg lg:text-2xl font-bold uppercase text-center">
-                        Where every guess brings <br /> you closer to the truth.
-                    </span>
+        <section>
+            <div className="h-screen w-screen">
+                <div className="mx-4 lg:mx-10 pt-5">
+                    <Button 
+                        variant="secondaryWhite"
+                        onClick={() => setIsOpen(true)}>
+                        How to play?
+                    </Button>
+                    <div className="flex flex-col items-center justify-center gap-8 lg:gap-4 mt-10 lg:mt-5">
+                        <h1>
+                            <img 
+                                src={Logo} 
+                                alt="logo" 
+                            />
+                        </h1>
+                        <span className="text-lg lg:text-2xl font-bold uppercase text-center">
+                            Where every guess brings <br/> you closer to the truth.
+                        </span>
+                    </div>
+                </div>
+                <div className="bg-white w-full h-[17vh] flex items-center justify-center rounded-tr-[35px] rounded-tl-[35px] absolute bottom-0">
+                    <Button 
+                        variant="primaryWhite" 
+                        onClick={handleNavigate}>
+                        Start
+                        </Button>
                 </div>
             </div>
-            <div className="bg-white w-full h-[20vh] lg:h-[17vh] flex items-center justify-center rounded-tr-[35px] rounded-tl-[35px] absolute bottom-0">
-                <Button 
-                    variant="primaryWhite" 
-                    onClick={handleNavigate}>
-                    Start
-                </Button>
+        
+
+            <aside className={clsx(
+                'fixed bottom-0 left-0 z-40 top-0 w-[460px] h-screen bg-white -translate-x-full transition-transform duration-300', 
+                isOpen ? 'translate-x-0' : '-translate-x-full')}>
+                <div className="relative">
+                    <div className="absolute top-4 right-6">
+                        <Button 
+                            variant="secondaryBlue"
+                            onClick={() => setIsOpen(false)}>
+                            Close
+                        </Button>
+                    </div>
+                </div>
+            </aside>
+
+            <div aria-hidden='true' onClick={() => setIsOpen(false)} className={clsx(
+                'fixed inset-0 bg-black/50 z-20',
+                'transition-opacity duration-300',
+                isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none')}>
             </div>
-        </div>
+        </section>
+
     )
 }
 
