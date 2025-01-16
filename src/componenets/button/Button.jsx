4f4727c578124dc7bc6de5202ba6f67e891da38a
mhanna <mandzyuk.annie@gmail.com>
2025-01-16 20:57:51 +0100

    Enhance UI components: Update Tailwind configuration

--- src/componenets/button/Button.jsx
+++ src/componenets/button/Button.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types'
 
 const Button = ({ variant, children, onClick }) => {
    const variantClasses = {
-    primaryWhite: "bg-blue-lighter text-white px-4 py-2 rounded-md shadow-lg font-bold uppercase transition-color duration-300 hover:bg-white hover:text-blue-lighter",
-    primaryBlue: "bg-white text-blue-lighter px-4 py-2 rounded-md shadow-lg font-bold uppercase transition-color duration-300 hover:bg-blue-lighter hover:text-white",
-    secondaryWhite: "bg-blue-lighter text-white font-bold uppercase p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full",
-    secondaryBlue: "bg-white text-blue-lighter font-bold uppercase p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-blue-lighter after:transition-all after:duration-300 hover:after:w-full",
+    primaryWhite: "bg-blue-lighter px-6 py-1 lg:px-9 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-white hover:text-blue-lighter text-white font-bold uppercase text-xl lg:text-2xl h",
+    primaryBlue: "bg-white text-blue-lighter px-6 py-1 lg:px-9 lg:py-2 rounded-md shadow-lg transition-color duration-300 hover:bg-blue-lighter hover:text-white font-bold uppercase text-xl lg:text-2xl",
+    secondaryWhite: "bg-blue-lighter p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full text-white font-bold uppercase text-sm lg:text-base",
+    secondaryBlue: "bg-white text-blue-lighter font-bold uppercase p-0 relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-blue-lighter after:transition-all after:duration-300 hover:after:w-full text-sm lg:text-base",
    }
 
    return (
