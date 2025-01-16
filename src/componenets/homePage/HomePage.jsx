2e32c6e64f632e11a2a60e04e99d0047bd54b768
mhanna <mandzyuk.annie@gmail.com>
2025-01-16 20:57:51 +0100

    Enhance UI components: Update Tailwind configuration

--- src/componenets/homePage/HomePage.jsx
+++ src/componenets/homePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import Button from '../button/Button'
+import Logo from '../../assets/logo.svg'
 
 const HomePage = () => {
     const navigate = useNavigate();
@@ -9,20 +10,31 @@ const HomePage = () => {
     };
 
     return (
-        <div className="h-screen w-[95vw] mt-10">
-            <Button variant="secondaryWhite">
-                How to play?
-            </Button>
-            <div>
-                <h1>LOGO</h1>
-                <span className="">
-                    Where every guess brings you closer to the truth.
-                </span>
+        <div className="h-screen w-screen">
+            <div className="mx-5 lg:mx-10 pt-5">
+                <Button 
+                    variant="secondaryWhite"
+                    onClick={handleNavigate}>
+                    How to play?
+                </Button>
+                <div className="flex flex-col items-center justify-center gap-8 lg:gap-4 mt-5">
+                    <h1>
+                        <img 
+                            src={Logo} 
+                            alt="logo" 
+                        />
+                    </h1>
+                    <span className="text-lg lg:text-2xl font-bold uppercase text-center">
+                        Where every guess brings <br /> you closer to the truth.
+                    </span>
+                </div>
             </div>
-            <div>
-                <button onClick={handleNavigate} className="">
-                    Get Started
-                </button>
+            <div className="bg-white w-full h-[20vh] lg:h-[17vh] flex items-center justify-center rounded-tr-[35px] rounded-tl-[35px] absolute bottom-0">
+                <Button 
+                    variant="primaryWhite" 
+                    onClick={handleNavigate}>
+                    Start
+                </Button>
             </div>
         </div>
     )
