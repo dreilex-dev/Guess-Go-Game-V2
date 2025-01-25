73d3a502527b215d8eea06e01445280100530fdb
Andrei Boaca <adi.boaca@gmail.com>
2025-01-25 15:16:49 +0100

    feat: before and after timer player cards display

---
+++ src/componenets/lobby/RevealedPlayerCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./revealedPlayerCard.css";
+
+const RevealedPlayerCard = ({ player }) => {
+  return (
+    <div className="player-card-container revealed">
+      <img
+        src={player.avatar}
+        alt="Player avatar"
+        className="player-avatar"
+      />
+      <div className="player-info">
+        <div className="playing-as revealed-info">
+          <span className="playing-as-label">Alias: </span>
+          {player.playingAs}
+        </div>
+        <div className="real-username show">
+          <span className="real-player-label">True Identity: </span>
+          <span className="username-text">{player.username}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RevealedPlayerCard;
